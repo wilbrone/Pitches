64d751dbf7d9fbd5dad421506cdafb61d85867cd
wilbrone <wilbroneokoth@gmail.com>
2019-11-24 02:09:55 +0300

    add authentication and saving user

--- app/auth/views.py
+++ app/auth/views.py
@@ -11,24 +11,26 @@ def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
         user = User.query.filter_by(email = login_form.email.data).first()
+
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
 
         flash('Invalid username or Password')
 
-    title = "One Minute Pitch login"
+    title = "One Minute Perfect Pitch login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/register',methods = ["GET","POST"])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,firstname= form.firstname.data,lastname= form.lastname.data,password = form.password.data)
+        user = User(email = form.email.data, username = form.username.data,full_name= form.full_name.data,password = form.password.data)
+        # saving the data
         db.session.add(user)
         db.session.commit()
 
-        mail_message("Welcome to One Minute Pitch","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to One Minute Perfect Pitch","email/welcome_user",user.email,user=user)
 
         return redirect(url_for('auth.login'))
         title = "New Account"
